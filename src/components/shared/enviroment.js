d6ae005a3355b2d3553cca367117f117851c3b41
Alex <alexokoro342@gmail.com>
2022-01-24 18:51:43 +0100

    update

--- src/components/shared/enviroment.js
+++ src/components/shared/enviroment.js
@@ -1,4 +1,4 @@
 export const enviroment = {
-    BASE_URL: "https://nucleusis-retail-api.herokuapp.com/",
+    BASE_URL: "https://nucleusis-retail-api.herokuapp.com/api/",
     API_KEY: "NUC-1BsOLIJwQyLrqkICdZIO-X"
 }
