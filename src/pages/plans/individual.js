b8403800f212d7424dce5880a50cb43549b7d590
Alex <alexokoro342@gmail.com>
2022-02-08 00:46:31 +0100

    update

--- src/pages/plans/individual.js
+++ src/pages/plans/individual.js
@@ -698,7 +698,7 @@ function Individual() {
                                             <tr>
                                                 <td className="p-4 border border-gray-200" colSpan="3"><span className="color-primary font-semibold md:text-lg text-base">Price</span>  <br /> <span className="text-black font-medium text-lg">N{planDetails?.plan.planAmount.amount}</span> </td>
                                             </tr>
-                                            {dependentArray.length && (
+                                            {dependentArray.length >= 1 && (
                                                 <>
                                                 {dependentArray.map((dependent, index) => (
                                                     <>
@@ -762,7 +762,7 @@ function Individual() {
                                                 <td className="p-4 border border-gray-200" colSpan="3"><span className="color-primary font-semibold md:text-base text-sm">Price</span>  <br /> <span className="text-black font-medium text-lg">N{planDetails?.plan.planAmount.amount}</span> </td>
                                             </tr>
 
-                                            {dependentArray.length && (
+                                            {dependentArray.length >= 1 && (
                                                 <>
                                                 {dependentArray.map((dependent, index) => (
                                                     <>
